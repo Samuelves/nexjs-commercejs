453f626d9d2befb862ff3c29505f2bc463982d67
Samuelves <ivessamuel64@gmail.com>
2021-02-16 16:11:27 -0300

    first commit

---
+++ pages/index.js
@@ -0,0 +1,42 @@
+import commerce from "../lib/commerce";
+import React from 'react'
+import Link from "next/link";
+import ProductList from "../components/ProductList";
+import CategoryList from "../components/CategoryList";
+export async function getStaticProps() {
+  const merchant = await commerce.merchants.about();
+  const { data: categories } = await commerce.categories.list();
+  const { data: products } = await commerce.products.list();
+
+  return {
+    props: {
+      merchant,
+      categories,
+      products,
+    },
+  };
+}
+
+export default function IndexPage({ merchant, categories, products }) {
+  return (
+    <React.Fragment>
+      <h1>{merchant.business_name}</h1>
+
+      <h3>
+        <Link href="/categories">
+          <a>Categories</a>
+        </Link>
+      </h3>
+
+      <CategoryList categories={categories} />
+
+      <h3>
+        <Link href="/products">
+          <a>Products</a>
+        </Link>
+      </h3>
+
+      <ProductList products={products} />
+    </React.Fragment>
+  );
+}
